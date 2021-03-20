08859b470a618bac81f6a328964a405641f3a131
Ankit <github@ankitmalik.in>
2021-03-20 19:10:45 +0530

    Update entrypoint.rs

--- stake-pool/program/src/entrypoint.rs
+++ stake-pool/program/src/entrypoint.rs
@@ -3,7 +3,7 @@
 /// 
 #![cfg(all(target_arch = "bpf", not(feature = "no-entrypoint")))]
 // Ask : Understand above statement in context of Solana 
-
+// !Ignore it
 
 use crate::{error::StakePoolError, processor::Processor};
 use solana_program::{
@@ -11,17 +11,31 @@ use solana_program::{
     program_error::PrintProgramError, pubkey::Pubkey,
 };
 
+
+
 // Ask : what does the ! here mean : syntactica?
 entrypoint!(process_instruction);
+// This is where my contract starts running
+// ankit sends a transaction to each blockchain -> validator runs this instruction 
 fn process_instruction(
     // David : What would be the typical input by the user here?
     // Similar to Solidity : a (contract) address of where this will be deployed? 
     // What's the ID being referred to in lib.rs at Line 20 
     // solana_program::declare_id!("poo1B9L9nR3CrcaziKVYVpRX6A9Y1LAXYasjjfCbApj");
 
+    // When you deploy this Stake-Pool contracts : you will get an address
+    // That address will be passed here
+    
+    // The canonical Stake Pool program has program ID : poo1B9L9nR3CrcaziKVYVpRX6A9Y1LAXYasjjfCbApj
+    // Other people can also deploy this => new program IDs => which would be here 
 
+    // 1 Stake Pool Program => Stake Pools (With Each Having an Owner) => Across Validator 
+    // David : 1 Stake Pool Program = 1 Stake Pool
+    // Ankit : I have my doubts, let's come back to this! 
     program_id: &Pubkey,
+    // At this point, Solana loads info of all accounts mentioned here into data structures of the form account:info
     accounts: &[AccountInfo],
+    // A reference/pointer to a u8 array 
     instruction_data: &[u8],
 ) -> ProgramResult {
     if let Err(error) = Processor::process(program_id, accounts, instruction_data) {
